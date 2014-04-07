8a4881c61db9dbf6712305727e14c4b39eb52401
Bryan Belanger <bbelanger@azcender.com>
2014-04-07 13:35:24 -0400

    Fixed manifest

--- puppet/manifests/apacheserver.pp
+++ puppet/manifests/apacheserver.pp
@@ -41,4 +41,14 @@ node apacheserver {
     ],
     docroot    => '/www',
   }
+
+  apache::vhost {'staging.azcender.com':
+    servername => 'staging.azcender.com',
+    vhost_name => '*',
+    port       => 80,
+    proxy_pass => [
+      { 'path' => '/', 'url' => 'http://192.168.168.50:8080/' },
+    ],
+    docroot    => '/www',
+  }
 }
