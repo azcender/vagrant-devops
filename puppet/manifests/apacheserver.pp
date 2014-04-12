121c20699ae98654e7129acb3ed5dfb8d98fb2f3
Bryan Belanger <bbelanger@azcender.com>
2014-04-11 23:53:37 -0400

    Ready for test on server

--- puppet/manifests/apacheserver.pp
+++ puppet/manifests/apacheserver.pp
@@ -2,53 +2,19 @@ node apacheserver {
 
   class { 'apache': }
 
-  apache::vhost {'sumit.azcender.com':
-    servername => 'sumit.azcender.com',
-    vhost_name => '*',
-    port       => 80,
-    proxy_pass => [
-      { 'path' => '/', 'url' => 'http://192.168.168.10:8080/' },
-    ],
-    docroot    => '/www',
+  file {['/var', '/var/yumrepos', '/var/cache', '/var/cache/yumrepos']:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  } 
+
+  apache::vhost {'yum-snapshot':
+    port    => '8280',
+    docroot => '/var/yumrepos/snapshot',
   }
 
-  apache::vhost {'tomcat.azcender.com':
-    servername => 'tomcat.azcender.com',
-    vhost_name => '*',
-    port       => 80,
-    proxy_pass => [
-      { 'path' => '/', 'url' => 'http://192.168.168.10:8080/' },
-    ],
-    docroot    => '/www',
-  }
-
-    apache::vhost {'jenkins.azcender.com':
-    servername => 'jenkins.azcender.com',
-    vhost_name => '*',
-    port       => 80,
-    proxy_pass => [
-      { 'path' => '/', 'url' => 'http://192.168.168.20:8080/' },
-    ],
-    docroot    => '/www',
-  }
-
-    apache::vhost {'artifactory.azcender.com':
-    servername => 'artifactory.azcender.com',
-    vhost_name => '*',
-    port       => 80,
-    proxy_pass => [
-      { 'path' => '/', 'url' => 'http://192.168.168.40:8081/' },
-    ],
-    docroot    => '/www',
-  }
-
-  apache::vhost {'staging.azcender.com':
-    servername => 'staging.azcender.com',
-    vhost_name => '*',
-    port       => 80,
-    proxy_pass => [
-      { 'path' => '/', 'url' => 'http://192.168.168.50:8080/' },
-    ],
-    docroot    => '/www',
+  createrepo { 'snapshot':
+    require => Apache::Vhost['yum-snapshot'],
   }
 }
